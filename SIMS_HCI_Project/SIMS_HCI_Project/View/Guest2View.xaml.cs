116571f195ec4c45971107b558c0da2368fd20ef
Anastasija Novakovic <anastasijanov2323@gmail.com>
2023-03-10 17:18:40 +0100

    Add views for main and search windows for Guest2

---
+++ SIMS_HCI_Project/SIMS_HCI_Project/View/Guest2View.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SIMS_HCI_Project.View
+{
+    /// <summary>
+    /// Interaction logic for Guest2View.xaml
+    /// </summary>
+    public partial class Guest2View : Window
+    {
+        public Guest2View()
+        {
+            InitializeComponent();
+        }
+
+
+        private void btnSearchReserve_Click(object sender, RoutedEventArgs e)
+        {
+            Window window = new TourSearchView();
+            window.Show();
+        }
+
+        private void DataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e) 
+        {
+            /// treba li ovo uopste?????
+        }
+
+    }
+
+    
+}
---
+++ SIMS_HCI_Project/SIMS_HCI_Project/View/TourSearchView.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SIMS_HCI_Project.View
+{
+    /// <summary>
+    /// Interaction logic for TourSearchView.xaml
+    /// </summary>
+    public partial class TourSearchView : Window
+    {
+        public TourSearchView()
+        {
+            InitializeComponent();
+        }
+
+        private void btnReserve_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnShowImages_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnSearch_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}

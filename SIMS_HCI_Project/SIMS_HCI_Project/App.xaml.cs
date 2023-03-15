e7e760674e4d4a2f4aff065cba89924d56538575
Anastasija Novakovic <anastasijanov2323@gmail.com>
2023-03-15 23:37:38 +0100

    Edit view for guest2

--- SIMS_HCI_Project/SIMS_HCI_Project/App.xaml.cs
+++ SIMS_HCI_Project/SIMS_HCI_Project/App.xaml.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.Globalization;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 
@@ -13,5 +15,14 @@ namespace SIMS_HCI_Project
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            //Set the culture for the entire application
+            CultureInfo culture = new CultureInfo("en-US");
+            Thread.CurrentThread.CurrentCulture = culture;
+            Thread.CurrentThread.CurrentUICulture = culture;
+
+            base.OnStartup(e);
+        }
     }
 }

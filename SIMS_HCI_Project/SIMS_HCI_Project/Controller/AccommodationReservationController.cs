105d9f5298d58e202b66324b50af9586a16cc2d6
MiljanaMa <miljana.marjanovic9@gmail.com>
2023-03-19 00:19:00 +0100

    Solve conflicts caused by merge

--- SIMS_HCI_Project/SIMS_HCI_Project/Controller/AccommodationReservationController.cs
+++ SIMS_HCI_Project/SIMS_HCI_Project/Controller/AccommodationReservationController.cs
@@ -65,10 +65,12 @@ namespace SIMS_HCI_Project.Controller
 
         public void Add(AccommodationReservation reservation, Guest1 guest)
         {
+            OwnerController ownerController = new OwnerController();
             reservation.Id = GenerateId();
             AccommodationReservation accommodationReservation = reservation;
             _reservations.Add(reservation);
             guest.Reservations.Add(reservation);
+            ownerController.AddReservationToOwner(reservation);
             Save();
             NotifyObservers();
         }

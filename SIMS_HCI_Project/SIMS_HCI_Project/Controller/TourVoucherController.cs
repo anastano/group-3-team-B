be4b22f4ace1707b413982583c4f6b36b25605d1
JelenaVujovic <33864251+JelenaVujovic@users.noreply.github.com>
2023-03-24 14:47:04 +0100

    Add voucher class, filehandler and controller with basic functionalities

---
+++ SIMS_HCI_Project/SIMS_HCI_Project/Controller/TourVoucherController.cs
@@ -0,0 +1,48 @@
+﻿using SIMS_HCI_Project.FileHandler;
+using SIMS_HCI_Project.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SIMS_HCI_Project.Controller
+{
+    public class TourVoucherController
+    {
+        private TourVoucherFileHandler _fileHandler;
+
+        private static List<TourVoucher> _tourVouchers;
+
+        public TourVoucherController()
+        {
+            _fileHandler = new TourVoucherFileHandler();
+
+            if (_tourVouchers == null)
+            {
+                Load();
+            }
+        }
+
+        public List<TourVoucher> GetAll()
+        {
+            return _tourVouchers;
+        }
+
+        public void Load()
+        {
+            _tourVouchers = _fileHandler.Load();
+        }
+
+        public void Save()
+        {
+            _fileHandler.Save(_tourVouchers);
+        }
+
+        private int GenerateId()
+        {
+            if (_tourVouchers.Count == 0) return 1;
+            return _tourVouchers[_tourVouchers.Count - 1].Id + 1;
+        }
+    }
+}
---
+++ SIMS_HCI_Project/SIMS_HCI_Project/FileHandler/TourVoucherFileHandler.cs
@@ -0,0 +1,32 @@
+﻿using SIMS_HCI_Project.Model;
+using SIMS_HCI_Project.Serializer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SIMS_HCI_Project.FileHandler
+{
+    public class TourVoucherFileHandler
+    {
+        private const string FilePath = "../../../Resources/Database/tourVouchers.csv";
+
+        private readonly Serializer<TourVoucher> _serializer;
+
+        public TourVoucherFileHandler()
+        {
+            _serializer = new Serializer<TourVoucher>();
+        }
+
+        public List<TourVoucher> Load()
+        {
+            return _serializer.FromCSV(FilePath);
+        }
+
+        public void Save(List<TourVoucher> tourVouchers)
+        {
+            _serializer.ToCSV(FilePath, tourVouchers);
+        }
+    }
+}

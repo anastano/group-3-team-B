3ccf71fe4dff1f9e52208d9f79c192576d373370
Anastasija Novakovic <anastasijanov2323@gmail.com>
2023-04-19 14:00:58 +0200

    Add Back option in TourReservationView

--- SIMS_HCI_Project/SIMS_HCI_Project/WPF/ViewModels/Guest2ViewModels/TourReservationViewModel.xaml.cs
+++ SIMS_HCI_Project/SIMS_HCI_Project/WPF/ViewModels/Guest2ViewModels/TourReservationViewModel.xaml.cs
@@ -22,10 +22,13 @@ namespace SIMS_HCI_Project.WPF.ViewModels.Guest2ViewModels
         private TourService _tourService;
         private TourReservationService _tourReservationService;
         private TourVoucherService _tourVoucherService;
-        #region Properties
+        #region Commands
         public RelayCommand ShowSuggestions { get; set; }
         public RelayCommand ConfirmReservation { get; set; }
+        public RelayCommand Back { get; set; }
+        #endregion
 
+        #region Properties
         public TourReservationView TourReservationView { get; set; }
         public TourVoucher TourVoucher { get; set; }
         public TourTime TourTime { get; set; }
@@ -131,6 +134,7 @@ namespace SIMS_HCI_Project.WPF.ViewModels.Guest2ViewModels
         public void InitCommands()
         {
             ConfirmReservation = new RelayCommand(Executed_ConfirmReservation, CanExecute_ConfirmReservation);
+            Back = new RelayCommand(Executed_Back, CanExecute_Back);
         }
         #region Commands
         private void Executed_ConfirmReservation(object sender) 
@@ -138,6 +142,17 @@ namespace SIMS_HCI_Project.WPF.ViewModels.Guest2ViewModels
             Reserve();
         }
         public bool CanExecute_ConfirmReservation(object sender)
+        {
+            return true;
+        }
+
+        private void Executed_Back(object sender)
+        {
+            Window window = new TourSearchView(Guest2);
+            window.Show();
+            TourReservationView.Close();
+        }
+        public bool CanExecute_Back(object sender)
         {
             return true;
         }

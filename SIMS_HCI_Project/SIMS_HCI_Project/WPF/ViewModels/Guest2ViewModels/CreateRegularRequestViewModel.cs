da7334ab1624af459ef9f73871a46f513c76d477
Anastasija Novakovic <anastasijanov2323@gmail.com>
2023-04-29 17:50:42 +0200

    Add views for tour requests

---
+++ SIMS_HCI_Project/SIMS_HCI_Project/WPF/ViewModels/Guest2ViewModels/CreateRegularRequestViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SIMS_HCI_Project.WPF.ViewModels.Guest2ViewModels
+{
+    public class CreateRegularRequestViewModel
+    {
+        public CreateRegularRequestViewModel()
+        {
+
+        }
+    }
+}

309f757b753c2a02dc4e9fce4a1307f73b20585f
MiljanaMa <miljana.marjanovic9@gmail.com>
2023-05-03 09:53:38 +0200

    Add star rating for profile view

---
+++ SIMS_HCI_Project/SIMS_HCI_Project/WPF/Converters/StarFillConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Markup;
+using System.Windows.Media;
+
+namespace SIMS_HCI_Project.WPF.Converters
+{
+    public class StarFillConverter : MarkupExtension, IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            double averageNumber = (double)value;
+            double starNumber = (double)parameter;
+            return starNumber <= averageNumber ? Brushes.Yellow : Brushes.Transparent;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return this;
+        }
+    }
+}
--- SIMS_HCI_Project/SIMS_HCI_Project/WPF/ViewModels/Guest1ViewModels/ProfileViewModel.cs
+++ SIMS_HCI_Project/SIMS_HCI_Project/WPF/ViewModels/Guest1ViewModels/ProfileViewModel.cs
@@ -5,22 +5,49 @@ using SIMS_HCI_Project.WPF.Views.OwnerViews;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace SIMS_HCI_Project.WPF.ViewModels.Guest1ViewModels
 {
-    internal class ProfileViewModel
+    internal class ProfileViewModel : INotifyPropertyChanged
     {
         private NotificationService _notificationService;
+        private RatingGivenByOwnerService _ratingService;
         public ObservableCollection<Notification> Notifications { get; set; }
         public Guest1 Guest { get; set; }
+        public string FullName { get; set; }
+        private double _averageRate;
+        public double AverageRate
+        {
+            get => _averageRate;
+            set
+            {
+                if (value != _averageRate)
+                {
+                    _averageRate = value;
+                    OnPropertyChanged(nameof(_averageRate));
+                }
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
         public ProfileViewModel(Guest1 guest)
         {
             _notificationService = new NotificationService();
+            _ratingService = new RatingGivenByOwnerService();
             Guest = guest;
+            //prebaciti u service
+            FullName = Guest.Name + " " + Guest.Surname;
             Notifications = new ObservableCollection<Notification>(_notificationService.GetUnreadByUserId(Guest.Id));
+            //potencijalna poruka ako nema obavjestenja
+            AverageRate = _ratingService.GetGuestAverageRate(Guest);
         }
     }
 }

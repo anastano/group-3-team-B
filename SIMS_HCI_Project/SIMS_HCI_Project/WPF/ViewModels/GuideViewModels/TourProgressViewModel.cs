cf9dc9c6e587936c207264cf0cb2f0a850ec1b26
JelenaVujovic <jelenaena.3a@gmail.com>
2023-06-05 21:44:07 +0200

    Add PDF Generate for guide

--- SIMS_HCI_Project/SIMS_HCI_Project/WPF/ViewModels/GuideViewModels/TourProgressViewModel.cs
+++ SIMS_HCI_Project/SIMS_HCI_Project/WPF/ViewModels/GuideViewModels/TourProgressViewModel.cs
@@ -1,4 +1,6 @@
-﻿using SIMS_HCI_Project.Applications.Services;
+﻿using iTextSharp.text.pdf;
+using iTextSharp.text;
+using SIMS_HCI_Project.Applications.Services;
 using SIMS_HCI_Project.Domain.Models;
 using SIMS_HCI_Project.WPF.Commands;
 using System;
@@ -10,6 +12,8 @@ using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.IO;
+using iTextSharp.text.pdf.draw;
 
 namespace SIMS_HCI_Project.WPF.ViewModels.GuideViewModels
 {
@@ -27,6 +31,7 @@ namespace SIMS_HCI_Project.WPF.ViewModels.GuideViewModels
         public RelayCommand MarkGuestPresent { get; set; }
         public RelayCommand EndTour { get; set; }
         public RelayCommand MoveKeyPoint { get; set; }
+        public RelayCommand DownloadPDF { get; set; }
         #endregion
 
         private TourTime _tour;
@@ -64,6 +69,7 @@ namespace SIMS_HCI_Project.WPF.ViewModels.GuideViewModels
             MarkGuestPresent = new RelayCommand(ExecutedMarkGuestPresentCommand, CanExecuteCommand);
             EndTour = new RelayCommand(ExecutedEndTourCommand, CanExecuteCommand);
             MoveKeyPoint = new RelayCommand(ExecutedMoveKeyPointCommand, CanExecuteCommand);
+            DownloadPDF = new RelayCommand(ExecutedDownloadPDFCommand, CanExecuteCommand);
         }
 
         private void ExecutedMarkGuestPresentCommand(object obj)
@@ -82,6 +88,122 @@ namespace SIMS_HCI_Project.WPF.ViewModels.GuideViewModels
         {
             _tourLifeCycleService.MoveToNextKeyPoint(Tour);
             LoadTour();
+        }
+
+        private void ExecutedDownloadPDFCommand(object obj)
+        {
+            Document document = new Document();
+            PdfWriter pdfWriter = PdfWriter.GetInstance(document, new FileStream("Tour" + "-" + Tour.Id + ".pdf", FileMode.Create));
+            document.Open();
+
+            Paragraph header = new Paragraph("GUEST LIST FOR TOUR " + Tour.Tour.Title);
+            header.Alignment = Element.ALIGN_CENTER;
+            header.SpacingAfter = 5f;
+            header.Font = FontFactory.GetFont(FontFactory.HELVETICA, 15);
+            document.Add(header);
+
+            LineSeparator ls = new LineSeparator();
+            document.Add(ls);
+
+            Paragraph tourInformation = new Paragraph("Tour information: ");
+            tourInformation.Alignment = Element.ALIGN_LEFT;
+            tourInformation.SpacingAfter = 2f;
+            tourInformation.SpacingBefore = 25f;
+            tourInformation.Font = FontFactory.GetFont(FontFactory.HELVETICA_BOLD, 13);
+            document.Add(tourInformation);
+
+            Paragraph tourTitle = new Paragraph(Tour.Tour.Title);
+            tourTitle.Alignment = Element.ALIGN_LEFT;
+            tourTitle.SpacingAfter = 2f;
+            tourTitle.SpacingBefore = 3f;
+            tourTitle.Font = FontFactory.GetFont(FontFactory.HELVETICA, 13);
+            document.Add(tourTitle);
+
+            Paragraph tourInfo = new Paragraph(Tour.Tour.Location.City + ", " + Tour.Tour.Location.Country);
+            tourTitle.Font = FontFactory.GetFont(FontFactory.HELVETICA, 10);
+            tourInfo.Alignment = Element.ALIGN_LEFT;
+            tourInfo.SpacingAfter = 2f;
+            document.Add(tourInfo);
+
+            tourInfo = new Paragraph(Tour.DepartureTime.ToString());
+            tourTitle.Font = FontFactory.GetFont(FontFactory.HELVETICA, 10);
+            tourInfo.Alignment = Element.ALIGN_LEFT;
+            tourInfo.SpacingAfter = 2f;
+            document.Add(tourInfo);
+
+            tourInfo = new Paragraph(Tour.Tour.Language);
+            tourTitle.Font = FontFactory.GetFont(FontFactory.HELVETICA, 10);
+            tourInfo.Alignment = Element.ALIGN_LEFT;
+            tourInfo.SpacingAfter = 20f;
+            document.Add(tourInfo);
+
+            float[] widths = { 10, 10, 10 };
+            PdfPTable table = new PdfPTable(widths);
+            table.DefaultCell.FixedHeight = 38f;
+
+            PdfPCell tableName = new PdfPCell(new Phrase("Guest List "));
+            tableName.Colspan = 3;
+            tableName.HorizontalAlignment = Element.ALIGN_CENTER;
+            tableName.VerticalAlignment = Element.ALIGN_MIDDLE;
+            table.AddCell(tableName);
+
+            PdfPCell header1 = new PdfPCell(new Phrase("Guest name"));
+            header1.HorizontalAlignment = Element.ALIGN_CENTER;
+            header1.VerticalAlignment = Element.ALIGN_MIDDLE;
+            header1.FixedHeight = 40f;
+            PdfPCell header2 = new PdfPCell(new Phrase("Guest attendance status"));
+            header2.HorizontalAlignment = Element.ALIGN_CENTER;
+            header2.VerticalAlignment = Element.ALIGN_MIDDLE;
+            header2.FixedHeight = 40f;
+            PdfPCell header3 = new PdfPCell(new Phrase("Guest joined at"));
+            header3.HorizontalAlignment = Element.ALIGN_CENTER;
+            header3.VerticalAlignment = Element.ALIGN_MIDDLE;
+            header3.FixedHeight = 40f;
+
+            table.AddCell(header1);
+            table.AddCell(header2);
+            table.AddCell(header3);
+
+            PdfPCell temp;
+            foreach (GuestTourAttendance guestTourAttendance in Tour.GuestAttendances)
+            {
+                temp = new PdfPCell(new Phrase(guestTourAttendance.TourReservation.Guest.Name + " " + guestTourAttendance.TourReservation.Guest.Surname));
+                temp.HorizontalAlignment = Element.ALIGN_CENTER;
+                temp.VerticalAlignment = Element.ALIGN_MIDDLE;
+                temp.FixedHeight = 38f;
+                table.AddCell(temp);
+                temp = new PdfPCell(new Phrase(guestTourAttendance.Status.ToString()));
+                temp.HorizontalAlignment = Element.ALIGN_CENTER;
+                temp.VerticalAlignment = Element.ALIGN_MIDDLE;
+                temp.FixedHeight = 38f;
+                table.AddCell(temp);
+                if (guestTourAttendance.KeyPointJoinedId != 0)
+                {
+                    temp = new PdfPCell(new Phrase(guestTourAttendance.KeyPointJoined.Title));
+                }
+                else
+                {
+                    temp = new PdfPCell(new Phrase("x"));
+                }
+                temp.HorizontalAlignment = Element.ALIGN_CENTER;
+                temp.VerticalAlignment = Element.ALIGN_MIDDLE;
+                temp.FixedHeight = 38f;
+                table.AddCell(temp);
+            }
+
+            PdfPCell total = new PdfPCell(new Phrase("Total number of guests: "));
+            total.Colspan = 2;
+            total.HorizontalAlignment = Element.ALIGN_RIGHT;
+            total.VerticalAlignment = Element.ALIGN_MIDDLE;
+            table.AddCell(total);
+            total = new PdfPCell(new Phrase(Tour.GuestAttendances.Count().ToString()));
+            total.HorizontalAlignment = Element.ALIGN_LEFT;
+            total.VerticalAlignment = Element.ALIGN_MIDDLE;
+            table.AddCell(total);
+
+            document.Add(table);
+
+            document.Close();
         }
 
         private void LoadTour()
